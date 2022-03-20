5a06d50b4f7119d30f254f2a7a6705c11485faa5
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-19 22:49:17 -0500

    fix bugs

--- test/mult.test.js
+++ test/mult.test.js
@@ -1,11 +1,10 @@
 const { mult } = require('../utils/math_testing');
 
-mult = jest.fn();
 
 describe('Math operation', () => {
 
     test('of a simple multiply', () => {
-        expect(mult(1, 0)).toBe(0);
+        expect(mult(1, 4)).toBe(4);
     })
 
 });
